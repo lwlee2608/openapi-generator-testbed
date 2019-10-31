26a968dfbab34d26d9e26659ddea3dc5bc62c8de
likwei <lwlee2608@gmail.com>
2019-11-01 01:13:42 +0800

    update run.sh script to use 4.2.0 openapi-generator

--- run.sh
+++ run.sh
@@ -3,12 +3,18 @@
 GENERATOR_CLI=./openapi-generator-cli.jar
 
 if [[ ! -f ${GENERATOR_CLI} ]]; then
-    wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/4.1.3/openapi-generator-cli-4.1.3.jar -O openapi-generator-cli.jar
+    wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/4.2.0/openapi-generator-cli-4.2.0.jar -O openapi-generator-cli.jar
 fi
 
 #OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000"
 
+SPEC=api.yaml
+GENERATOR=java-vertx-web
+OUT_DIR=java-vertx-web
+
+[[ -d $OUT_DIR ]] && rm -rf $OUT_DIR
+
 java $OPTS -jar $GENERATOR_CLI generate \
-         -i api.yaml \
-         -g java \
-         -o example
+         -i $SPEC \
+         -g $GENERATOR \
+         -o $OUT_DIR
