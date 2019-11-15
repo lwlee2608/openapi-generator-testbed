79657c94a74de6a9b66aa7a4fdf58c212dea5bc3
likwei <lwlee2608@gmail.com>
2019-11-15 18:13:56 +0800

    add additionalProperties ignoreAnyOfInEnum

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ if [[ ! -f ${GENERATOR_CLI} ]]; then
     wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/4.2.0/openapi-generator-cli-4.2.0.jar -O openapi-generator-cli.jar
 fi
 
+unset OPTS
 #OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000"
 
 SPEC=TS29507_Npcf_AMPolicyControl.yaml
@@ -17,4 +18,6 @@ OUT_DIR=java-vertx-web
 java $OPTS -jar $GENERATOR_CLI generate \
          -i $SPEC \
          -g $GENERATOR \
-         -o $OUT_DIR
+         -o $OUT_DIR \
+         --additional-properties ignoreAnyOfInEnum=true
+
