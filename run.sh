793f6194af146beb8ac2b5e03cefdba03d9520c6
likwei <lwlee2608@gmail.com>
2019-11-19 23:10:40 +0800

    update version

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-GENERATOR_CLI=./openapi-generator-cli.jar
-
-if [[ ! -f ${GENERATOR_CLI} ]]; then
-    wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/4.2.0/openapi-generator-cli-4.2.0.jar -O openapi-generator-cli.jar
-fi
-
 unset OPTS
 #OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000"
 
+GENERATOR_CLI=./openapi-generator-cli.jar
 SPEC=TS29507_Npcf_AMPolicyControl.yaml
 GENERATOR=java-vertx-web
 OUT_DIR=java-vertx-web
+VERSION=4.2.1
+
+
+if [[ ! -f ${GENERATOR_CLI} ]]; then
+    wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${VERSION}/openapi-generator-cli-${VERSION}.jar -O openapi-generator-cli.jar
+fi
 
 [[ -d $OUT_DIR ]] && rm -rf $OUT_DIR
 
