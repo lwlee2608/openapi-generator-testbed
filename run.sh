a3240e98eed9f2d0e5cce14202f452d2cc4a5f0e
Jason Lee <lwlee2608@gmail.com>
2021-03-10 18:23:43 +0800

    add 3gpp spec files

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000"
 
 GENERATOR_CLI=./openapi-generator-cli.jar
-SPEC=petstore.yaml
+SPEC=TS32291_Nchf_ConvergedCharging.yaml
 GENERATOR=java-vertx-web
 OUT_DIR=java-vertx-web
 VERSION=5.0.1
@@ -13,13 +13,8 @@ if [[ ! -f ${GENERATOR_CLI} ]]; then
     wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${VERSION}/openapi-generator-cli-${VERSION}.jar -O openapi-generator-cli.jar
 fi
 
-if [[ ! -f ${SPEC} ]]; then
-    wget https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/modules/openapi-generator/src/test/resources/3_0/petstore.yaml
-    SPEC=petstore.yaml
-fi
 
 [[ -d $OUT_DIR ]] && rm -rf $OUT_DIR
-
 
 java $OPTS -jar $GENERATOR_CLI generate \
          -i $SPEC \
